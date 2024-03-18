762ece8664a5fd2a046ab92c3ecb5234924603e1
wow <110139562+yty-yang@users.noreply.github.com>
2024-03-18 21:02:35 +0800

    finished ice-webdev2

--- ice/ice-webdev2/starter/src/components/AllHurricanes.jsx
+++ ice/ice-webdev2/starter/src/components/AllHurricanes.jsx
@@ -1,9 +1,8 @@
-
 // You'll probably need these imports...
 
-import { useEffect, useState } from "react"
+import {useEffect, useState} from "react"
 import Hurricane from "./Hurricane";
-import { Col, Container, Pagination, Row } from "react-bootstrap";
+import {Col, Container, Pagination, Row} from "react-bootstrap";
 
 export default function AllHurricanes() {
 
@@ -20,7 +19,42 @@ export default function AllHurricanes() {
     // 5. Fix your key error!
     // 6. Paginate so that up to 6 elements are displayed on a page.
 
+    const [hurricanes, setHurricanes] = useState([]);
+    const [page, setPage] = useState(1);
+
+    useEffect(() => {
+        fetch("https://cs571.org/api/s24/ice/hurricanes", {
+            headers: {
+                "X-CS571-ID": "bid_7aa1690b9dabcac6bf2f5fbe49c240e522b2f17232ff01a835fdda35db5cf438"
+            }
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data);
+                setHurricanes(data);
+            })
+    }, [])
+
     return <div>
         <h1>Hurricane Finder</h1>
+
+        <Container>
+            <Row>
+                {
+                    hurricanes.slice((page - 1) * 6, page * 6).map(h =>
+                        <Col key={h.id} xs={12} md={6} lg={4}>
+                            <Hurricane {...h}/>
+                        </Col>
+                    )
+                }
+            </Row>
+        </Container>
+
+        <Pagination>
+            <Pagination.Item active={page === 1} onClick={() => setPage(1)}>1</Pagination.Item>
+            <Pagination.Item active={page === 2} onClick={() => setPage(2)}>2</Pagination.Item>
+            <Pagination.Item active={page === 3} onClick={() => setPage(3)}>3</Pagination.Item>
+            <Pagination.Item active={page === 4} onClick={() => setPage(4)}>4</Pagination.Item>
+        </Pagination>
     </div>
 }
--- ice/ice-webdev2/starter/src/components/Hurricane.jsx
+++ ice/ice-webdev2/starter/src/components/Hurricane.jsx
@@ -1,3 +1,13 @@
+import {Container} from "react-bootstrap";
+
 export default function Hurricane(props) {
-    return <h2>I am a hurricane!</h2>
+    const startDate = new Date(props.start_date).toLocaleString();
+    const endDate = new Date(props.end_date).toLocaleString();
+
+    return <div>
+        <h2>{props.name}</h2>
+        <p>Category {props.category}</p>
+        <p>Started on {startDate}</p>
+        <p>Ended on {endDate}</p>
+    </div>
 }
