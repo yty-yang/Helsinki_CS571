f6031bfdc8664ce40840a9d8eceb315a9688e6e9
wow <110139562+yty-yang@users.noreply.github.com>
2024-03-19 21:04:04 +0800

    download hw4

---
+++ hw/hw4/src/components/Classroom.jsx
@@ -0,0 +1,26 @@
+import { Button, Container, Form, Row } from "react-bootstrap";
+
+const Classroom = () => {
+    return <div>
+        <h1>Badger Book</h1>
+        <p>Search for students below!</p>
+        <hr />
+        <Form>
+            <Form.Label htmlFor="searchName">Name</Form.Label>
+            <Form.Control id="searchName"/>
+            <Form.Label htmlFor="searchMajor">Major</Form.Label>
+            <Form.Control id="searchMajor"/>
+            <Form.Label htmlFor="searchInterest">Interest</Form.Label>
+            <Form.Control id="searchInterest"/>
+            <br />
+            <Button variant="neutral">Reset Search</Button>
+        </Form>
+        <Container fluid>
+            <Row>
+                { /* TODO Students go here! */ }
+            </Row>
+        </Container>
+    </div>
+}
+
+export default Classroom;
